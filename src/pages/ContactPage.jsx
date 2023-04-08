23d8e12342e73f68bb09b3bdf5762cf04f42a984
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-04-09 01:37:49 +0200

    added useEffect to get the movie details as well as the crew details

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function ContactPage() {
   return (
+    
+    <div className="under-construction">
     <div>ContactPage</div>
+    <img src={"images/under_con.png"} alt="under-construction" />
+  </div>
   )
 }
 
--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -1,13 +1,94 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 function InfoPage() {
   const { movieId } = useParams();
   console.log("movie-id --->", movieId);
+  const [oneMovie, setOneMovie] = useState([]);
+  const [crew, setCrew] = useState([])
+  const [fetching, setFetching] = useState(true);
+
+  // to calculate movie rating
+  //   v = 459 (number of votes)
+  // m = 25000 (minimum number of votes required to be listed in IMDb's Top Rated Movies)
+  // R = 6.856 (average rating)
+  // C = 6.9 (mean vote across the whole report)
+
+  // ((v / (v + m)) * R) + ((m / (v + m)) * C) = ((459 / (459 + 25000)) * 6.856) + ((25000 / (459 + 25000)) * 6.9)
+
+  // fetch(`https://api.themoviedb.org/3/movie/<<movieID>>/credits?api_key=<<your_api_key>>`)
+  //             .then(response => response.json())
+  //             .then((jsonData)=>jsonData.crew.filter(({job})=> job ==='Director'))
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=4d4f24c1de9b6106c77077a3305aa28f&language=en-US`
+      )
+      .then((response) => {
+        console.log(response.data);
+        const data = response.data;
+        setOneMovie(data);
+        setFetching(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [movieId]);
+
+  useEffect(() => {
+    axios(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=4d4f24c1de9b6106c77077a3305aa28f&language=en-US`
+    ).then((response)=>{
+      console.log(response.data)
+      const data = response.data;
+      setCrew(data);
+
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }, [movieId]);
+
   return (
     <>
       <div>InfoPage</div>
-      <Link to='/home'>Back</Link>
+      {fetching && (
+        <img src={"/images/spinner2.gif"} alt="spinner" className="spinner" />
+      )}
+
+      {!fetching && (
+        <>
+          <img
+            src={
+              "https://image.tmdb.org/t/p/original" + `${oneMovie.poster_path}`
+            }
+            style={{ width: "300px", height: "400px" }}
+          />
+          <h3>{oneMovie.title}</h3>
+          <p>movie rating</p>
+          <span>
+            {(oneMovie.vote_count / (oneMovie.vote_count + 25000)) *
+              oneMovie.vote_average +
+              (25000 / (oneMovie.vote_count + 25000)) * 6.9}
+          </span>
+          <p>{oneMovie.release_date}</p>
+
+          <ul>
+            {oneMovie.genres.map((genre) => {
+              return <li key={genre.id}>{genre.name}</li>;
+            })}
+          </ul>
+
+          <p>{oneMovie.overview}</p>
+          <h6>Featured Crew</h6>
+          <p>movie length: {oneMovie.runtime}</p>
+          <p></p>
+        </>
+      )}
+
+      <Link to="/home">Back</Link>
     </>
   );
 }
