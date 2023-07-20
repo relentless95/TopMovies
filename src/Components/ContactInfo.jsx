3ecc4e1feb2e73e1dc361ef36e601312d18f948b
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-07-20 20:08:56 +0200

    feat: updated the contact page

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -1,8 +1,35 @@
-import react, { useState } from "react";
+import react, { useRef, useState } from "react";
 import { MdOutgoingMail } from "react-icons/md";
+import emailjs from "@emailjs/browser";
 
 function ContactDetails() {
   const [name, setName] = useState("");
+  const form = useRef();
+
+  console.log("id---->", typeof import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form.current,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
     <div className="contact-div">
       <div className="contact-text">
@@ -10,16 +37,17 @@ function ContactDetails() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
       <div className="contact-form">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <label className="label-contact">
             Full Name<b>*</b>
           </label>
           <input
             type="text"
-            placeholder="e.g:Felix Schmidt"
+            placeholder="e.g Felix Schmidt"
             value={name}
             onChange={(e) => setName(e.target.value)}
             className="input-contact"
+            name="user_name"
           />
 
           <label className="label-contact">
@@ -29,6 +57,17 @@ function ContactDetails() {
             type="email"
             placeholder="e.g Youremail@gmail.com"
             className="input-contact"
+            name="user_email"
+          />
+
+          <label className="label-contact">
+            subject <b>*</b>
+          </label>
+          <input
+            type="text"
+            placeholder="e.g Subject"
+            className="input-contact"
+            name="subject"
           />
 
           <label className="label-contact">
@@ -37,6 +76,7 @@ function ContactDetails() {
           <textarea
             className="textarea-contact"
             placeholder="Write here..."
+            name="message"
           ></textarea>
           <button type="submit" className="button-contact">
             <MdOutgoingMail /> &nbsp; Send Message
